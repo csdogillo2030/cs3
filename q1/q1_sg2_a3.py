78ebfe082c595dcfc0a1edd2dda0fb1a39495e19
csdogillo2030 <cadogillo2030@clc.pshs.edu.ph>
2026-08-15 12:23:20 +0800

    more stuff
    Added greeting and farewell messages to the zodiac program.

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -7,6 +7,8 @@ August 15, 2026
 '''
 import time
 
+print("Hello!")
+
 zodiac= ["(Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)", "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
 year= int(input("Enter your birth year: "))
 
@@ -18,3 +20,4 @@ else:
     print("Your Chinese Zodiac sign is: ", zodiac[x])
     
 time.sleep(0.5)
+print("Goodbye!")
