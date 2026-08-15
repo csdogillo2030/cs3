e5755691fab9dedc74bb53664710301a51f014ae
csdogillo2030 <cadogillo2030@clc.pshs.edu.ph>
2026-08-15 12:13:12 +0800

    code

---
+++ q1/q1_sg2_a3.py
@@ -0,0 +1,16 @@
+'''
+
+#22 Charlize Sky A. Dogillo
+9 - Balingkilat
+August 15, 2026
+
+'''
+
+zodiac= ["(Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)", "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
+year= int(input("Enter your birth year: "))
+
+if year<= 1899:
+    print("Invalid Year, it should not be earlier than 1900.")
+else:
+    x= (year - 2020) % 12
+    print("Your Chinese Zodiac sign is: ", zodiac[x])
