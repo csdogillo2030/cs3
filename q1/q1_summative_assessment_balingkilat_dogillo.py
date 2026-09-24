a87f97ff94f9acf0c27161cb29110421b4537723
csdogillo2030 <cadogillo2030@clc.pshs.edu.ph>
2026-09-24 10:52:37 +0800

    im dying

---
+++ q1/q1_summative_assessment_balingkilat_dogillo.py
@@ -0,0 +1,55 @@
+'''
+Charlize Sky A. Dogillo
+September 24, 2026
+'''
+
+#bank, acc, savings acc
+#i probably wont finish this on time
+print("Welcome to Metrobank")
+
+class Bank:
+  def __init__(self, name, number, type, balance= 0, accounts):
+    self.accounts = Account()
+  def openAccount(self):
+    print("Ready to open an account")
+    accname = str(input("Account Name: "))
+    accnumber = str(input("Account Number: "))
+    acctype = str(input("Account Type (savings or checking): "))
+    print("Account Created")
+    print(f"{self.name) [{self.number}] P {self.__balance}")
+    
+  def showAccounts(self):
+    
+  def deposit(self):
+    
+  def addInterest():
+    
+  def closeAccount():
+
+  def __del__(self):
+    
+
+class Account:
+  def __init__(name, number, type, balance= 0, accounts):
+    self.name = accname
+    self.number = accnumber
+    self.type = acctype
+    self.__balance = balance
+    self.accounts = 
+    
+
+class SavingsAccount(Account):
+  def __init__(self, name, number, balance= 0, interest= 0.05):
+    super().__init__(name, number, balance= 0)
+    self.interest = interest
+  
+
+mbtc = Bank("Metrobank")
+mbtc.openAccount()
+mbtc.openAccount()
+mbtc.showAccounts()
+mbtc.deposit()
+mbtc.deposit()
+mbtc.addInterest()
+mbtc.closeAccount()
+del bank #(add later)
